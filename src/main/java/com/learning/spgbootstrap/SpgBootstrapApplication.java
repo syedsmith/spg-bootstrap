eb4b7f5d37402944c941d522f1ebcd3c7f74994e
Smith <smith6101994@gmail.com>
2022-05-12 10:52:19 +0530

    spring maven  project created

---
+++ src/main/java/com/learning/spgbootstrap/SpgBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spgbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spgbootstrap/SpgBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spgbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
