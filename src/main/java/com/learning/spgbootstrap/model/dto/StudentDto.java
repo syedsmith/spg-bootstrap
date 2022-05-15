de620b9f55c5580074134201cd8b7892790038a2
Smith <smith6101994@gmail.com>
2022-05-15 09:00:47 +0530

    Adding first route
    post route added

---
+++ src/main/java/com/learning/spgbootstrap/model/dto/StudentDto.java
@@ -0,0 +1,15 @@
+package com.learning.spgbootstrap.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentDto {
+    public String name;
+    public String age;
+}
